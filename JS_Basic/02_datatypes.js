4d4ded8557162aae58340635f2fd369b15b9bf1c
rudra-123-patel <rudrapatel1054@gmail.com>
2025-04-16 16:46:13 +0530

    added variables

---
+++ JS_Basic/02_datatypes.js
@@ -0,0 +1,14 @@
+let name = "rudra";
+let age = 18;
+let isLoggedIn = false;
+let state = null; // no value assigned yet
+
+//number
+//bigint
+//string
+//boolean
+//null => Standalone value
+//undefined => value not defined
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
